b4f2a513f000b3fd939683b54d222b8658173a99
Abhijat Malviya <malviya.abhijat@gmail.com>
2013-06-10 08:40:27 -0700

    placeholder

--- test.cpp
+++ test.cpp
@@ -4,8 +4,15 @@
 using std::cout;
 using std::endl;
 
+void test_func()
+{
+    SpreadsheetCell spCell(5);
+    cout << spCell.getValue() << endl;
+}
+
 int main()
 {
+    test_func();
     cout << "A basic test has passed" << endl;    
     return 0;
 }
